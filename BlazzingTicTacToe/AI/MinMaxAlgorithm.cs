b8fd76337f214d2d06dcef5df74fb8a668abdad7
= <=>
2023-05-14 07:29:03 +0530

    initial Commit

---
+++ BlazzingTicTacToe/AI/MinMaxAlgorithm.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazzingTicTacToe.AI
+{
+    public class MinMaxAlgorithm
+    {
+
+        public class Turn
+        {
+            public int row, col;        }
+    };
+
+    private static readonly char Player = 'x';
+    private static readonly char Opponent = 'o';
+
+}
